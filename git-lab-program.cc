023943e71790efed0d50b28ea46b806447bf686c
Gage-58 <gc383325@ohio.edu>
2026-09-02 16:03:47 -0400

    Code runs with no problem

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gage Cromwell
+ *        Date: 9/2/26
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
